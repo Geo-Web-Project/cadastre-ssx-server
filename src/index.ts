0be912234a06e4cd66193224d3edf05a52558e70
Cody Hatfield <cody.hatfield@me.com>
2023-02-01 14:51:10 -0800

    Add /storage/delegation endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,12 @@
-import express, { Express } from "express";
+import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import { SSXServer, SSXExpressMiddleware } from "@spruceid/ssx-server";
+import { create } from "@web3-storage/w3up-client";
+import { StoreConf } from "@web3-storage/access";
+import * as UCANDID from "@ipld/dag-ucan/did";
+import { CarWriter } from "@ipld/car";
+import { Readable } from "stream";
 
 dotenv.config();
 
@@ -18,6 +23,11 @@ const ssx = new SSXServer({
   },
 });
 
+const store = new StoreConf({
+  profile: process.env.W3_STORE_NAME ?? "cadastre-ssx-server",
+});
+const w3upClientP = create({ store });
+
 app.use(
   cors({
     credentials: true,
@@ -27,21 +37,41 @@ app.use(
 
 app.use(SSXExpressMiddleware(ssx));
 
-// app.get("/userdata", async (req: Request, res: Response) => {
-//   if (!req.ssx.verified) {
-//     return res.status(401).json({ success: false, message: "Unauthorized" });
-//   }
-//
-//   const data = await getDataFromNode(req.ssx.siwe?.address);
-//
-//   res.json({
-//     success: true,
-//     userId: req.ssx.siwe?.address,
-//     message:
-//       "Some user data, retrieved from a blockchain node the server can access.",
-//     ...data,
-//   });
-// });
+app.post("/storage/delegation", async (req: Request, res: Response) => {
+  if (!req.ssx.verified) {
+    return res.status(401).json({ success: false, message: "Unauthorized" });
+  }
+
+  if (!req.ssx.userId) {
+    return res
+      .status(400)
+      .json({ success: false, message: "Could not parse userId from session" });
+  }
+
+  if (!req.body.aud) {
+    return res
+      .status(400)
+      .json({ success: false, message: "Missing required 'aud' from body" });
+  }
+
+  const w3upClient = await w3upClientP;
+  const did = UCANDID.parse(req.body.aud);
+  const delegation = await w3upClient.createDelegation(did, [
+    "upload/add",
+    "store/add",
+  ]);
+
+  const { writer, out } = CarWriter.create([delegation.root.cid as any]);
+  // @ts-ignore
+  for (const block of delegation.export()) {
+    writer.put(block);
+  }
+
+  writer.close();
+
+  res.setHeader("Content-Type", "application/vnd.ipld.car");
+  Readable.from(out).pipe(res);
+});
 
 app.use((req, res) => {
   if (!res.headersSent) {
