d354662d213cd10ddd4030dc2eeb2ec4166e420e
Cody Hatfield <cody.hatfield@me.com>
2023-02-09 15:03:25 -0800

    Add DOMAIN env var

--- src/index.ts
+++ src/index.ts
@@ -36,6 +36,11 @@ const ssx = new SSXServer({
       apiKey: process.env.SSX_API_TOKEN ?? "",
     },
     sessionConfig: {
+      sessionOptions: {
+        cookie: {
+          domain: process.env.DOMAIN,
+        },
+      },
       store: (session) => {
         const redisStore = ConnectRedis(session);
         return new redisStore({
