d84458dc37a684b4a7389340544aeb1740a04756
Cody Hatfield <cody.hatfield@me.com>
2023-04-17 15:03:21 -0700

    feat: Claim Referral UCAN, remove SSX endpoints
    * Working on adding a UCAN for claim referral
    
    * Remove SSX endpoints, add Claim Referral delegation

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,15 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
 import cors from "cors";
-import { SSXServer, SSXExpressMiddleware } from "@spruceid/ssx-server";
+import { SiweMessage } from "siwe";
 import { create } from "@web3-storage/w3up-client";
 import { StoreConf } from "@web3-storage/access";
 import * as UCANDID from "@ipld/dag-ucan/did";
+import * as UCAN from "@ipld/dag-ucan";
 import { CarWriter } from "@ipld/car";
 import { Readable } from "stream";
+import * as Block from "multiformats/block";
+import { sha256 as hasher } from "multiformats/hashes/sha2";
 
 dotenv.config();
 
@@ -14,25 +17,6 @@ const app: Express = express();
 const port = process.env.PORT || 3001;
 
 app.set("trust proxy", process.env.TRUST_PROXY ?? "loopback");
-
-const ssx = new SSXServer({
-  signingKey: process.env.SSX_SIGNING_KEY,
-  providers: {
-    metrics: {
-      service: "ssx",
-      apiKey: process.env.SSX_API_TOKEN ?? "",
-    },
-    sessionConfig: {
-      sessionOptions: {
-        cookie: {
-          domain: process.env.SESSION_DOMAIN,
-          sameSite: "none",
-          secure: true,
-        },
-      },
-    },
-  },
-});
 
 const store = new StoreConf({
   profile: process.env.W3_STORE_NAME ?? "cadastre-ssx-server",
@@ -49,9 +33,10 @@ app.use(
   })
 );
 
-app.use(SSXExpressMiddleware(ssx));
+app.use(express.json());
 
-app.post("/storage/delegation", async (req: Request, res: Response) => {
+// Get a delegation for W3up storage
+app.post("/delegations/storage", async (req: Request, res: Response) => {
   if (!req.body) {
     res.status(422).json({ message: "Expected body." });
     return;
@@ -66,47 +51,94 @@ app.post("/storage/delegation", async (req: Request, res: Response) => {
     res.status(422).json({ message: "Expected the field `siwe` in the body." });
     return;
   }
-  let ssxLoginResponse;
-  try {
-    ssxLoginResponse = await ssx.login(
-      req.body.siwe,
-      req.body.signature,
-      req.body.daoLogin,
-      req.body.resolveEns,
-      req.session.nonce,
-      req.body.resolveLens
-    );
-  } catch (error: any) {
-    return res.status(500).json({ message: error.message });
-  }
-  const { success, error } = ssxLoginResponse;
-  if (!success) {
-    let message = error.type;
-    if (error.expected && error.received) {
-      message += ` Expected: ${error.expected}. Received: ${error.received}`;
-    }
-    return res.status(400).json({ message });
-  }
 
-  if (!req.body.aud) {
-    return res
-      .status(400)
-      .json({ success: false, message: "Missing required 'aud' from body" });
+  const { siwe, signature } = req.body;
+  const siweMessage = new SiweMessage(siwe);
+
+  try {
+    await siweMessage.verify({ signature: signature });
+  } catch (error: any) {
+    return res.status(401).json({ message: error.message });
   }
 
   const w3upClient = await w3upClientP;
-  const did = UCANDID.parse(req.body.aud);
-  const delegation = await w3upClient.createDelegation(
-    did,
+  const didAud = UCANDID.parse(siweMessage.uri);
+
+  // Issue UCAN for w3up
+  const w3UpDelegation = await w3upClient.createDelegation(
+    didAud,
     ["upload/add", "store/add"],
     { expiration: Math.floor(Date.now() / 1000) + 86400 } // +24 hours
   );
 
-  const { writer, out } = CarWriter.create([delegation.root.cid as any]);
+  const { writer, out } = CarWriter.create([w3UpDelegation.root.cid as any]);
   // @ts-ignore
   for (const block of delegation.export()) {
     writer.put(block);
   }
+
+  writer.close();
+
+  res.setHeader("Content-Type", "application/vnd.ipld.car");
+  Readable.from(out).pipe(res);
+});
+
+// Get a delegation for Claim Referral
+app.post("/delegations/claim-referral", async (req: Request, res: Response) => {
+  if (!req.body) {
+    res.status(422).json({ message: "Expected body." });
+    return;
+  }
+  if (!req.body.signature) {
+    res
+      .status(422)
+      .json({ message: "Expected the field `signature` in body." });
+    return;
+  }
+  if (!req.body.siwe) {
+    res.status(422).json({ message: "Expected the field `siwe` in the body." });
+    return;
+  }
+
+  const { siwe, signature } = req.body;
+  const siweMessage = new SiweMessage(siwe);
+
+  try {
+    await siweMessage.verify({ signature: signature });
+  } catch (error: any) {
+    return res.status(401).json({ message: error.message });
+  }
+
+  const w3upClient = await w3upClientP;
+  const didAud = UCANDID.parse(siweMessage.uri);
+  const didPkh = UCANDID.parse(
+    `did:pkh:eip155:${siweMessage.chainId}:${siweMessage.address}`
+  );
+
+  // Issue UCAN for claim referral
+  const claimReferralDelegation = await UCAN.issue({
+    issuer: w3upClient.agent(),
+    audience: didAud,
+    lifetimeInSeconds: 60 * 60 * 24 * 14,
+    capabilities: [
+      {
+        can: "http/post",
+        with: `https://geoweb.network/claim/${didPkh.did()}`,
+      },
+    ],
+  });
+  const claimReferralDelegationBlock = await Block.encode({
+    value: claimReferralDelegation,
+    codec: UCAN,
+    hasher,
+  });
+
+  const { writer, out } = CarWriter.create([claimReferralDelegationBlock.cid]);
+
+  writer.put({
+    cid: claimReferralDelegationBlock.cid,
+    bytes: claimReferralDelegationBlock.bytes,
+  });
 
   writer.close();
 
